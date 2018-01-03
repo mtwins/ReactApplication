6e08eda7b487f193a6608c94bdc0ea9da3714c12
Melissa Heredia <fq2514@wayne.edu>
2018-01-03 13:30:34 -0500

    Second Commit
    Second commit-added styling to project and readme

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # ReactApplication
+I have made a simple react application, which has 2 views.
+The user is able to navigate from the first view to the second view using 
+a button on the screen. The user can go back to the first view using the back button
+on the upper left of the screen.
+
+The coding aspect of the application wasn't too dificult and there were many tutorials 
+that showed how to use react navigation. After I got the project running, 
+I created a stack navigator for the project and added
+two screens a home screen and second page and added them to the stack. After the two screen were
+created, I added a button that would link to the second page to the home page. I then changed 
+the styling on the pages to make it look nicer.
+
+I found the hardest part of the project was setting up the application.I had to install
+many different applications and programs and I also had to learn how to use the react CLI.
+There were many little issues with dependancies I had to figure out to actual get my project
+running on the simulator.
+How I solved this problem was being very careful about following the recommended 
+installtion steps.
---
+++ firstReactApplication/App.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import { StyleSheet, Text, View,Button } from 'react-native';
+import { StackNavigator } from 'react-navigation';
+
+class HomeScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Welcome To My First Application'
+  };
+  render() {
+     const { navigate } = this.props.navigation;
+     return (
+       <View style={styles.container}>
+         <Text  style={{fontWeight: 'bold'}}>Hello and Welcome!</Text>
+         <Button
+		 color="#841584"
+           onPress={() => navigate('SecondPage')}
+           title="Click to navigate to the next page"
+         />
+       </View>
+     );
+   }
+}
+
+class SecondPage extends React.Component {
+  static navigationOptions = {
+    title: 'SecondPage',
+  };
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text >Hello World</Text>
+      </View>
+    );
+  }
+}
+
+const SimpleApp = StackNavigator({
+  Home: { screen: HomeScreen },
+   SecondPage: { screen: SecondPage },
+});
+
+export default class App extends React.Component {
+  render() {
+    return <SimpleApp />;
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+	//fontWeight: 'bold'
+  }
+});
